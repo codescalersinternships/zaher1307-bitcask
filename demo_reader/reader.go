b6c0327605303d11a4c61d61a6a7cf7ceb90d482
zaher1307 <ahmedzaher1307@gmail.com>
2022-07-25 12:48:03 +0200

    add demo files for using bitcask

---
+++ demo_reader/reader.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"bitcask"
+	"fmt"
+	"path"
+)
+
+func main() {
+	bc, err := bitcask.Open(path.Join("bitcask"))
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	fmt.Println("****** Get some items ********")
+	val26, _ := bc.Get("key26")
+	fmt.Printf("value of key26 is: %s\n", string(val26))
+
+	val10, _ := bc.Get("key10")
+	fmt.Printf("value of key10 is: %s\n", string(val10))
+
+	val89, _ := bc.Get("key89")
+	fmt.Printf("value of key89 is: %s\n", string(val89))
+
+	bc.Close()
+}
---
+++ demo_writer/writer.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bitcask"
+	"fmt"
+	"path"
+)
+
+func main() {
+	bc, err := bitcask.Open(path.Join("bitcask"), bitcask.ReadWrite)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	fmt.Println("****** append 100 item ******")
+	for i := 0; i < 100; i++ {
+		key := "key" + fmt.Sprintf("%d", i)
+		value := "value" + fmt.Sprintf("%d", i)
+		bc.Put(key, value)
+	}
+
+	fmt.Println("****** merge old files ******")
+	bc.Merge()
+
+	fmt.Println("****** Get some items ******")
+	val2, _ := bc.Get("key2")
+	fmt.Printf("value of key2 is: %s\n", string(val2))
+
+	val15, _ := bc.Get("key15")
+	fmt.Printf("value of key15 is: %s\n", string(val15))
+
+	val77, _ := bc.Get("key77")
+	fmt.Printf("value of key77 is: %s\n", string(val77))
+
+	bc.Close()
+}
