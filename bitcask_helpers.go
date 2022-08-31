9fde8bc46886a82a4f695e09dc15b643a9bdf9e7
zaher1307 <ahmedzaher1307@gmail.com>
2022-08-31 12:09:51 +0200

    fix un-neccesary intermediate memory allocations

--- bitcask_helpers.go
+++ bitcask_helpers.go
@@ -161,8 +161,7 @@ func (b *Bitcask) buildKeyDirFile() error {
 		tstampStr := padWithZero(recValue.tstamp)
 		keySizeStr := padWithZero(int64(len(key)))
 
-		line := fileIdStr + valueSizeStr + valuePosStr + tstampStr + keySizeStr + key
-		fmt.Fprintln(keyDirFile, line)
+		fmt.Fprintln(keyDirFile, fileIdStr + valueSizeStr + valuePosStr + tstampStr + keySizeStr + key)
 	}
 	return nil
 }
